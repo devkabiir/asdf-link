9053622af2954968f8e720e305f921d765da0908
devkabiir <18462563+devkabiir@users.noreply.github.com>
2022-09-17 14:10:37 +0530

    Add asdf plugin command

---
+++ lib/commands/command.bash
@@ -0,0 +1,58 @@
+#!/usr/bin/env bash
+#
+# description:
+#   Use system tools with asdf version manager
+#
+# usage:
+#   asdf link <tool-name> <version=latest> <...paths=$PWD>"
+#
+# examples:
+#   asdf link zls master ./bin
+#   asdf link zls master
+#   asdf link zls
+#   asdf link rustc nightly ./bin ./Cargo/packages/bin
+#
+# notes:
+#   ---
+#
+
+set -e
+
+shopt -s nullglob
+
+function _asdf_link_system_tool_if_not_exists() {
+    local tool=$1
+    if ! asdf plugin list | grep -q "$tool"; then
+        asdf plugin add "$tool" https://github.com/devkabiir/asdf-link
+    fi
+}
+
+function main() {
+    ARGS=("$@")
+    tool="${ARGS[0]}"
+    version="${ARGS[1]:-latest}"
+    source_paths=("${ARGS[@]:2}")
+
+    # default to current directory
+    if [ -z "${source_paths// /}" ]; then
+        source_paths=("$PWD")
+    fi
+
+    _asdf_link_system_tool_if_not_exists "$tool"
+
+    # if the version already exists, we remove it to clear stale shims.
+    if asdf list "$tool" "$version" | grep -q "$version"; then
+        asdf uninstall "$tool" "$version"
+    fi
+    asdf install "$tool" "$version"
+
+    dest_path=$(asdf where "$tool" "$version")/bin/
+
+    echo Creating shims...
+    for source_path in "${source_paths[@]}"; do
+        ln -sv "$(realpath $source_path)"/* "$dest_path"
+    done
+    asdf reshim "$tool" "$version"
+}
+
+main "$@"
